b1f618229c28d3a8c24d3cf778090558eb661d44
Kamemax <mak6267@gmail.com>
2024-12-16 16:31:06 +0700

    Add project files.

---
+++ DBMSSQ2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace DBMSSQ2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvOrders = new DataGridView();
+            dgvDetail = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvOrders).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvDetail).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvOrders
+            // 
+            dgvOrders.AllowUserToAddRows = false;
+            dgvOrders.AllowUserToDeleteRows = false;
+            dgvOrders.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvOrders.Location = new Point(12, 96);
+            dgvOrders.Name = "dgvOrders";
+            dgvOrders.ReadOnly = true;
+            dgvOrders.RowHeadersWidth = 51;
+            dgvOrders.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvOrders.Size = new Size(571, 366);
+            dgvOrders.TabIndex = 0;
+            dgvOrders.CellContentClick += dataGridView1_CellContentClick;
+            dgvOrders.CellMouseUp += dgvOrders_CellMouseUp;
+            // 
+            // dgvDetail
+            // 
+            dgvDetail.AllowUserToAddRows = false;
+            dgvDetail.AllowUserToDeleteRows = false;
+            dgvDetail.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDetail.Location = new Point(655, 96);
+            dgvDetail.Name = "dgvDetail";
+            dgvDetail.ReadOnly = true;
+            dgvDetail.RowHeadersWidth = 51;
+            dgvDetail.Size = new Size(547, 366);
+            dgvDetail.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(243, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 20);
+            label1.TabIndex = 2;
+            label1.Text = "ใบสั้งซื่อ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(909, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(123, 20);
+            label2.TabIndex = 3;
+            label2.Text = "รายระเอียดการสั้งชื่อ";
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1214, 486);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dgvDetail);
+            Controls.Add(dgvOrders);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvOrders).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvDetail).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvOrders;
+        private DataGridView dgvDetail;
+        private Label label1;
+        private Label label2;
+    }
+}
